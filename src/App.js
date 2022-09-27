4632055d4712dae5686669a54a4227dd468cc63e
Adelina <adelhetman@gmail.com>
2022-09-27 18:57:58 +0300

    created the structure

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, {useState} from "react";
+import './App.css';
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
+
+export default function App() {
+  let [query, setQuery] = useState("");
+  let [weather, setWeather] = useState({});
+  let [city, setCity] = useState("");
+
+  function showWeather(response) {
+    console.log(response.data);
+    setQuery(true);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: response.data.weather[0].icon,
+      data: new Date(response.data.dt * 1000),
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function search() {
+    let apiKey = "1dad91bc92f6c69698e1aad50d0a7304";
+    let units = `metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(showWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let searchForm = (
+    <div className="Weather m-3">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city"
+          className="me-3"
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+
+  if (query) {
+    return (
+      <div>
+       {searchForm}
+        <h3>The weather in {weather.city} is:</h3>
+        <WeatherInfo info={weather} />
+      </div>
+    );
+  } else {
+    search();
+    return (
+      <div>
+       {searchForm}
+      </div>
+    );
+  }
+}
